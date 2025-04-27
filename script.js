81ad7128e4fe4a1221e84f76f481529f1ec1351f
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-27 19:40:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function getDOMLevel(element){
+	let level = 0;
+	let current = element
+
+	while(current.parentNode){
+		current = current.parentNode
+		level++
+	}
+	return level
+}
+const element = document.getElementById('level')
+const level = getDOMLevel(element)
+alert(`The level of the element is: ${level}`)
